84ee663418fe2882e68a875b64383c6458660f3a
Rob <robzyb+altgithub@gmail.com>
2021-08-28 12:33:46 +1000

    First attempt

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,11 @@ import SerialPort from 'serialport'
 import prompt from 'prompt';
 import { SpeeduinoComm } from 'speeduino-comm'
 import { Command } from 'commander';
+import { hasUncaughtExceptionCaptureCallback } from 'process';
 const program = new Command();
 
 program.option('-d, --device <dev>', 'serial device')
 program.parse(process.argv)
-
-async function logLPS(portPath: string, interval: number) {
-    // Setup the required connections
-    const speedy = new SpeeduinoComm({ path: portPath, options: { baudRate: 115200, autoOpen: false } })
-    speedy.raw.on('error', () => console.log("Serial port error"))
-    speedy.raw.on('unexpected', (data) => { console.log("Unexpected data:", data); throw "ERROR" })
-
-    // Make sure we can connect
-    try {
-        await new Promise<void>((resolve, reject) => speedy.open((err) => err ? reject(err) : resolve()))
-    } catch (err) {
-        console.log("Couldn't connect:", err)
-        return
-    }
-
-    // Display the signatures
-    try {
-        await speedy.signature().then((response) => {
-            console.log("Signature:", response)
-        })
-        await speedy.versionInfo().then((response) => {
-            console.log("Version info:", response)
-        })
-    } catch (err) {
-        console.log("Error on info:", err.message)
-    }
-
-    while (true) {
-        const everyOneSecond = new Promise<void>(resolve => setTimeout(resolve, interval))
-        try {
-            const response = await speedy.loopsPerSecond()
-            console.log((new Date).toISOString(), "Loops per second:", response)
-        } catch (err) {
-            console.log("Error on loopsPerSecond:", err.message)
-            return
-        }
-        await everyOneSecond
-    }
-}
-
 
 async function promptForDevicePath(): Promise<string> {
     const ports = await SerialPort.list()
@@ -83,13 +44,27 @@ async function main() {
         portPath = await promptForDevicePath()
     }
 
-    const continuallLogCPS = async (interval: number) => {
-        while (true) {
-            await logLPS(portPath, interval)
-            await new Promise<void>(resolve => setTimeout(resolve, interval))
+    const speedy = new SpeeduinoComm({ path: portPath, options: { baudRate: 115200, autoOpen: false } })
+    speedy.raw.on('error', () => { throw "Serial port error" })
+    speedy.raw.on('unexpected', (data) => { console.log("Unexpected data:", data); throw "ERROR" })
+
+    speedy.open(err => {
+        if (err) {
+            throw "Error opening"
         }
-    }
-    continuallLogCPS(1000)
+        speedy.startToothLogger().then(() => {
+            console.log("Tooth logger started");
+            return new Promise<void>(resolve => setTimeout(resolve, 5000))
+        }).then(async () => {
+            while (true) {
+                const teethTime = await speedy.toothLog()
+                for (let t of teethTime) {
+                    console.log(t);
+                }
+                await new Promise<void>(resolve => setTimeout(resolve, 5000))
+            }
+        })
+    })
 }
 
 main()
